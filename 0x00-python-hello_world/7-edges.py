0bc5f6138cdce8f1b35435a5e97daab50e6b5124
vicuartas230 <vicuartas230@gmail.com>
2021-04-26 23:41:46 +0000

    Fixing name of variables

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word1 = word[:3]
-word2 = word[7:]
-word3 = word[1:8]
-print("First 3 letters: {}".format(word1))
-print("Last 2 letters: {}".format(word2))
-print("Middle word: {}".format(word3))
+word_first_3 = word[:3]
+word_last_2 = word[7:]
+middle_word = word[1:8]
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))

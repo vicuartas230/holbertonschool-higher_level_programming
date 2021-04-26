0698d0a14b0c58f3d621757385dcf03788d6fad0
vicuartas230 <vicuartas230@gmail.com>
2021-04-26 23:30:12 +0000

    Uploading file of task 7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word1 = "Hol"
+word2 = "on"
+word3 = "olberto"
+print("First 3 letters: {}".format(word1))
+print("Last 2 letters: {}".format(word2))
+print("Middle word: {}".format(word3))

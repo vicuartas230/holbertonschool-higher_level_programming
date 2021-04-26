f2734dc559d39bd3040444f4877e586ff4344a21
vicuartas230 <vicuartas230@gmail.com>
2021-04-26 23:36:55 +0000

    Fixing variables

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word1 = "Hol"
-word2 = "on"
-word3 = "olberto"
+word1 = word[:3]
+word2 = word[7:]
+word3 = word[1:8]
 print("First 3 letters: {}".format(word1))
 print("Last 2 letters: {}".format(word2))
 print("Middle word: {}".format(word3))

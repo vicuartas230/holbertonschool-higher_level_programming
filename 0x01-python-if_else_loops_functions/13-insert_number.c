40fd78281cff513cec046a0d76b00d1b29a288cb
vicuartas230 <vicuartas230@gmail.com>
2021-04-28 04:02:14 +0000

    Fixing the order of number

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,29 +1,37 @@
 #include "lists.h"
 
 /**
- * insert_node - This function inserts a number
- * into a sorted singly linked list.
- * @head: The start of the linked list.
- * @number: The number to insert.
- * Return: the address of the new node, or NULL if it failed.
- */
+  * insert_node - This function inserts a number
+  * into a sorted singly linked list.
+  * @head: The start of the linked list.
+  * @number: The number to insert.
+  * Return: the address of the new node, or NULL if it failed.
+  */
 
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *tmp = NULL, *new = NULL;
-	unsigned int i = 0;
 
 	tmp = *head;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
-	while (i < 4)
+	while (tmp)
 	{
+		if (tmp->next == NULL)
+		{
+			new->next = NULL;
+			tmp->next = new;
+			break;
+		}
+		if (tmp->n < number && number < tmp->next->n)
+		{
+			new->next = tmp->next;
+			tmp->next = new;
+			break;
+		}
 		tmp = tmp->next;
-		i++;
 	}
-	new->next = tmp->next;
-	tmp->next = new;
 	return (new);
 }

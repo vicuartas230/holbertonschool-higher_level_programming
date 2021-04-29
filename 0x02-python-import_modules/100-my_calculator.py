72127501ad08395591ceb0d22612c7155ecf4ecf
vicuartas230 <vicuartas230@gmail.com>
2021-04-29 03:20:27 +0000

    Removing the variable result

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,17 +9,13 @@ else:
     a = int(s[1])
     b = int(s[3])
     if s[2] == '+':
-        result = add(a, b)
-        print("{} {} {} = {}".format(s[1], s[2], s[3], result))
+        print("{} {} {} = {}".format(s[1], s[2], s[3], add(a, b)))
     elif s[2] == '-':
-        result = sub(a, b)
-        print("{} {} {} = {}".format(s[1], s[2], s[3], result))
+        print("{} {} {} = {}".format(s[1], s[2], s[3], sub(a, b)))
     elif s[2] == '*':
-        result = mul(a, b)
-        print("{} {} {} = {}".format(s[1], s[2], s[3], result))
+        print("{} {} {} = {}".format(s[1], s[2], s[3], mul(a, b)))
     elif s[2] == '/':
-        result = div(a, b)
-        print("{} {} {} = {}".format(s[1], s[2], s[3], result))
+        print("{} {} {} = {}".format(s[1], s[2], s[3], div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)

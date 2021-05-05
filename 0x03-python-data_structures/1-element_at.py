ca1b3586e944e507c219e1785ef3f3e5e03b4ac2
victor <vicuartas230@gmail.com>
2021-05-05 16:36:40 -0500

    Fixing limits in second statement

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return None
     elif idx == 0 and len(my_list) == 0:
         return None

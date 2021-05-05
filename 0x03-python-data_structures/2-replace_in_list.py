2d94f67cd631d706e8ffecefc957fc0a6a3eeb41
victor <vicuartas230@gmail.com>
2021-05-05 16:38:49 -0500

    Removing the loop

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,10 +2,8 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return my_list
     else:
-        for i in my_list:
-            if i == idx:
-                my_list[i] = element
-                return my_list
+        my_list[idx] = element
+        return my_list

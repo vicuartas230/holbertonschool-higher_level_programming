4ef9ea02c52d568cf86ddc57d6989f5b44d00c67
victor <vicuartas230@gmail.com>
2021-05-05 22:11:09 -0500

    Changing some ways to add numbers to the tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_x = 0, 0
     if len(tuple_a) < 2:
-        tuple_a = tuple_a + tuple_x
+        list_a = list(tuple_a)
+        list_a.append(0)
+        tuple_a = tuple(list_a)
         tuple_c = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     elif len(tuple_b) < 2:
-        tuple_b = tuple_b + tuple_x
+        list_b = list(tuple_b)
+        list_b.append(0)
+        list_b.append(0)
+        tuple_b = tuple(list_b)
         tuple_c = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     else:
         tuple_c = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]

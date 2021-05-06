8220569b182ae0b60180f94a0348e2ce60d7a89d
victor <vicuartas230@gmail.com>
2021-05-05 20:42:40 -0500

    Adding statement when sentence is empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple_a = len(sentence), sentence[0]
+    if (len(sentence) > 0):
+        tuple_a = len(sentence), sentence[0]
+    else:
+        tuple_a = len(sentence), None
     return tuple_a

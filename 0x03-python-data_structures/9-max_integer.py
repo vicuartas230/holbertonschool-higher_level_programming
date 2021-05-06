60790de8bc530eb79eae6f86d21ce463f1ad5a48
victor <vicuartas230@gmail.com>
2021-05-05 20:06:42 -0500

    Fixing statement

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_i = my_list[0]
     if len(my_list) == 0:
         return None
-    for i in my_list:
-        if i > max_i:
-            max_i = i
-    return max_i
+    else:
+        max_i = my_list[0]
+        for i in my_list:
+            if i > max_i:
+                max_i = i
+        return max_i

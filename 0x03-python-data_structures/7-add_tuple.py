79beef3ab48d562a5ea797129550f878deabddb0
victor <vicuartas230@gmail.com>
2021-05-05 22:12:45 -0500

    securing empty tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,6 +3,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 2:
         list_a = list(tuple_a)
         list_a.append(0)
+        list_a.append(0)
         tuple_a = tuple(list_a)
         tuple_c = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     elif len(tuple_b) < 2:

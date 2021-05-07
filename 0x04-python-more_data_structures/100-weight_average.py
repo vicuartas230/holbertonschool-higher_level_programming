0846cb934b473e95e81d3519f57c960391b01e5e
victor <vicuartas230@gmail.com>
2021-05-06 23:02:00 -0500

    Adding zero statement

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    return sum(list(map(lambda x: x[0] * x[1], my_list))) / sum(list(
-        map(lambda x: x[1], my_list)))
+    return 0 if not len(my_list) else sum(list(map(
+        lambda x: x[0] * x[1], my_list))) / sum(list(
+            map(lambda x: x[1], my_list)))

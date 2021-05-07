11230cdb7e9477461d9d1841a3c5aab872fce593
victor <vicuartas230@gmail.com>
2021-05-06 20:42:44 -0500

    Fixing some structure in code

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,14 +2,12 @@
 def roman_to_int(roman_string):
     if roman_string == '':
         return 0
-    romans = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50, 'X\
-C': 90, 'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
-    if roman_string in romans:
-        return romans[roman_string]
-    else:
-        res = 0
-        roman = list(roman_string)
-        for i in roman:
-            if i in romans.keys():
-                res += romans[i]
-        return res
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    for i in range(len(roman_string)):
+        if i != len(roman_string) - 1 and (
+                romans[roman_string[i + 1]] > romans[roman_string[i]]):
+            res -= romans[roman_string[i]]
+        else:
+            res += romans[roman_string[i]]
+    return res

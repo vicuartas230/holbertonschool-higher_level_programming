af0dedc44b6f328e0016ac2c76a8101ec104b72a
victor <vicuartas230@gmail.com>
2021-05-17 02:05:33 -0500

    Adding try and except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,12 @@
 def safe_print_list(my_list=[], x=0):
     printed = 0
     for i in range(x):
-        if my_list[i]:
-            print("{}".format(my_list[i]), end='')
-            printed += 1
-        else:
+        try:
+            for i in range(x):
+                print("{}".format(my_list[i]), end='')
+                printed += 1
+            print('')
             return printed
-    print('')
-    return printed
+        except IndexError:
+            print('')
+            return printed

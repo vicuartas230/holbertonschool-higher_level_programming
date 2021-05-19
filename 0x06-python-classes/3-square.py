964f28ea721f4db2c80efc6b0ee67dc526617609
victor <vicuartas230@gmail.com>
2021-05-18 22:55:49 -0500

    Uploading file of task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This program defines a class Square. """
+
+
+class Square:
+    """ A class to represent a Square. """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        x = self.__size
+        return x * x

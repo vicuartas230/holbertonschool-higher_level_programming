38ade3a1fe076450c5e2b03ad3f8e42afa86753a
victor <vicuartas230@gmail.com>
2021-05-18 22:51:10 -0500

    Fixing order of statements

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """ This program defines a class Square. """
+
+
 class Square:
     """ A class to represent a Square. """
     def __init__(self, size=0):
-        if size < 0:
-            raise ValueError
         if type(size) != int:
-            raise TypeError
-        try:
-            self.__size = size
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

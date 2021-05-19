c9685963697a63923966d7554542778e7845aa96
victor <vicuartas230@gmail.com>
2021-05-19 14:13:34 -0500

    Adding comments

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" This program defines a class MagicClass """
+
+
 class MagicClass:
+    """ Class to return the circumference area """
     def __init__(self, radius=0):
         if type(radius) is not int or type(radius) is not float:
             raise TypeError("radius must be a number")

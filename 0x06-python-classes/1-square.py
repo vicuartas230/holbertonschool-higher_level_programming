00cc79808843f3b832f86de6e95381f3ee5f7567
victor <vicuartas230@gmail.com>
2021-05-18 22:42:34 -0500

    Uploading file of task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Program This program defines a class Square. """
+
+
+class Square:
+    """ A class to represent a Square. """
+    def __init__(self, size=None):
+        self.__size = size

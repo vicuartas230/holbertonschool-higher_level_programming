82e5f3e4580dbde98f888c02ab6b26a0eaa0bb1e
victor <vicuartas230@gmail.com>
2021-05-18 22:39:25 -0500

    Fixing comments

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# This program defines a empty class Square.
+""" This program defines a empty class Square. """
 
 
 class Square:
-    # A class to represent a Square.
+    """ A class to represent a Square. """
     pass

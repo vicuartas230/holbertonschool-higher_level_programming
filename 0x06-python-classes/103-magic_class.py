0c97916979427be32b44598fa02e95d853d06c7d
victor <vicuartas230@gmail.com>
2021-05-19 16:14:41 -0500

    Changing or for and

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,7 +7,7 @@ import math
 class MagicClass:
     """ Class to return the circumference area """
     def __init__(self, radius=0):
-        if type(radius) is not int or type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 

a12f74acb9af62c04256d3f027216edba8e31683
victor <vicuartas230@gmail.com>
2021-05-19 19:37:52 -0500

    Adding other except

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,8 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError:
-        print("Exception: Unknown format code 'd' \
-for object of type 'str'", file=sys.stderr)
+        print("Exception: Unknown format code 'd' for object of type 'str'", file=sys.stderr)
+        return False
+    except TypeError:
+        print("Exception: unsupported format string passed to set.__format__", file=sys.stderr)
         return False

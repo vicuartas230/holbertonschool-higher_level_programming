1ddb8627a6f843a6a0dfca78cf83779d7bcfd632
victor <vicuartas230@gmail.com>
2021-05-19 20:05:29 -0500

    Uploading file of task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    res = 0
+    new_list = []
+    for i in range(list_length):
+        try:
+            res = int(my_list_1[i]) / int(my_list_2[i])
+        except ZeroDivisionError:
+            print("division by 0")
+            res = 0
+        except ValueError:
+            print("wrong type")
+            res = 0
+        except IndexError:
+            print("out of range")
+            res = 0
+        finally:
+            new_list.append(res)
+    return new_list

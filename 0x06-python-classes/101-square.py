177a7c38c36d2a8b7de7ed356c198b8f8060c043
victor <vicuartas230@gmail.com>
2021-05-19 23:12:21 -0500

    Uploading file of task 8

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+""" This program defines a class Square. """
+
+
+class Square:
+    """ A class to represent a Square. """
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if len(position) != 2 or type(position[0]) != int or type(
+                position[1]) != int or position[1] < 0 or position[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__size = size
+        self.__position = position
+    
+    def __repr__(self):
+        return str(self.my_print())
+    def __str__(self):
+        return str(self.my_print())
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if len(value) != 2 or type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if x[0] < 0 or x[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        x = self.__size
+        return x * x
+
+    def my_print(self):
+        length = self.__size
+        if not length:
+            print('')
+        else:
+            for m in range(self.__position[1]):
+                print('')
+            for i in range(length):
+                for k in range(self.__position[0]):
+                    print(' ', end='')
+                for j in range(length):
+                    print('#', end='')
+                print('')
+        return ''

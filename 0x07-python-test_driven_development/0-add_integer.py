a7a7a8357d8d56303c50ae3541eede10a17030f1
victor <vicuartas230@gmail.com>
2021-05-25 22:39:40 -0500

    Changing the statements

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,9 +7,9 @@ def add_integer(a, b=98):
     """ This function returns the add of two integers
         a is the first number to add
         b is the second number to add """
-    if type(a) is not [int, float]:
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) is not [int, float]:
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)

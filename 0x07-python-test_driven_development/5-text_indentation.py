8a18f089a5130a323b0ab818623d8b1504b757c0
victor <vicuartas230@gmail.com>
2021-05-25 23:13:24 -0500

    Fixing some spaces around the text

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,15 +8,31 @@ def text_indentation(text):
     """ This function prints a text with 2 new lines
         after each of these characters: '.', '?' and ':'
         text is a string """
+    comp = 1
     if type(text) is not str:
         raise TypeError("text must be a string")
     separator = '.:?'
     letter = 0
     while letter in range(len(text)):
-        for char in separator:
-            if text[letter] == char:
-                print("{}{}".format(text[letter], '\n'))
-                letter += 2
-                break
-        print("{}".format(text[letter]), end='')
-        letter += 1
+        try:
+            for char in separator:
+                if text[letter] == char:
+                    comp = 0
+                    print("{}{}".format(text[letter], '\n'))
+                    letter += 1
+                    break
+            if text[letter + 1] and text[letter + 1] != ' \
+' and text[letter] == ' ' and not comp:
+                comp = 1
+                letter += 1
+            elif text[letter] == ' ' and text[letter + 1] == ' ':
+                comp = 0
+                letter += 1
+                continue
+            elif comp:
+                print("{}".format(text[letter]), end='')
+                letter += 1
+        except IndexError:
+            if text[letter] != ' ':
+                print("{}".format(text[letter]), end='')
+            break

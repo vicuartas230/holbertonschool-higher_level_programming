b499c1269a70806c50712877b75607b93a158f13
victor <vicuartas230@gmail.com>
2021-06-01 18:35:13 -0500

    Adding documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+""" This program defines a function lookup """
+
+
 def lookup(obj):
+    """ This function returns the list of available
+        attributes and methods of an object """
     return obj.__dict__

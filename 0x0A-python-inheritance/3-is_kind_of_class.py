7900ef194ed4ff8c37c2a7789dcbf36bfc10913a
victor <vicuartas230@gmail.com>
2021-06-01 18:53:16 -0500

    Fixing style error

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ This program defines a function to check
     if an object is an instance of a class """
+
+
 def is_kind_of_class(obj, a_class):
     """ This function returns True if the object
         is an instance of, or if the object is an

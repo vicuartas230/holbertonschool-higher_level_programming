c6fd3be6092570af37b6e186d16129974a33f6af
victor <vicuartas230@gmail.com>
2021-06-01 19:30:46 -0500

    Adding documentation

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+""" This program defines a class BaseGeometry """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
+    """ This class defines the exception errors to
+        geometry methods """
     def __init__(self, width, height):
         super().integer_validator("width", width)
         super().integer_validator("height", height)

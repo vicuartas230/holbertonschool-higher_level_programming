ffc41975bece9776534a36253cff0458d87f7ee7
victor <vicuartas230@gmail.com>
2021-06-01 19:03:34 -0500

    Adding documentation

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" This program defines a class BaseGeometry """
+
+
 class BaseGeometry:
+    """ This class will be used to define methods
+        in next tasks """
     def area(self):
         raise Exception("area() is not implemented")

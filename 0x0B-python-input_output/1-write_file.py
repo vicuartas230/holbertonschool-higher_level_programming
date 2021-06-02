d41dab2ea61b9d0c1b5cff9b0a37176be273af61
victor <vicuartas230@gmail.com>
2021-06-02 13:04:03 -0500

    Uploading file of task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This program defines a function write_file """
+
+def write_file(filename="", text=""):
+    """ This function writes a string to a text file (UTF8)
+        and returns the number of characters written """
+    with open(filename, 'w') as new:
+        writting = new.write(text)
+    new.close()
+    return writting

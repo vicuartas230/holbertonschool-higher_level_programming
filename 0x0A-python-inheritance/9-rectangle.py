0966127c4eef6f1e21b48d77415d74f69edf0794
victor <vicuartas230@gmail.com>
2021-06-01 19:37:50 -0500

    Moving the documentation

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """ This program defines a class Rectangle """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):

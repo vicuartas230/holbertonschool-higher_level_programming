fc75117a5c39e0c02147f5037f5cad11e2bb35d8
victor <vicuartas230@gmail.com>
2021-06-02 17:27:28 -0500

    Uploading file of task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This program efines a function load_from_json_file """
+import json
+
+
+def load_from_json_file(filename):
+    """ This function creates an Object from a “JSON file” """
+    with open(filename, 'r') as new:
+        return json.load(new)

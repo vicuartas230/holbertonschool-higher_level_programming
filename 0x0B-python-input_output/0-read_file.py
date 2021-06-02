f7de15075cd24f5f6d586334a271a4c3153bee59
victor <vicuartas230@gmail.com>
2021-06-02 13:21:57 -0500

    Fixing style

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ This program defines a function read_file """
 
+
 def read_file(filename=""):
     """ This function reads a text file (UTF8) and prints it to stdout """
     with open(filename, 'r') as new:
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ This program defines a function write_file """
 
+
 def write_file(filename="", text=""):
     """ This function writes a string to a text file (UTF8)
         and returns the number of characters written """

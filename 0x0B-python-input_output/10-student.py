7587042ba824b2cca540c7fa55e12ea636dcbde2
victor <vicuartas230@gmail.com>
2021-06-02 23:57:58 -0500

    Uploading file of task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This program defines a class Student """
+
+
+class Student:
+    """ This class defines public attributes of a student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            new_dict = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    new_dict.update({i: getattr(self, i)})
+            return new_dict
+        else:
+            return self.__dict__

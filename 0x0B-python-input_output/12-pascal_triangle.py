9c7b92338b6ede4fe5d7cdef0d8cecf6b0a5973f
victor <vicuartas230@gmail.com>
2021-06-03 16:22:27 -0500

    Uploading file of task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This program defines a function pascal_triangle """
+
+
+def pascal_triangle(n):
+    """ This function returns a list of lists of integers
+        representing the Pascal’s triangle of n """
+    lines = []
+    for row in range(1, (n + 1)):
+        elemts = []
+        item = 1
+        for column in range(1, (row + 1)):
+            elemts.append(item)
+            item = int(item * (row - column) / column)
+        lines.append(elemts)
+    return lines

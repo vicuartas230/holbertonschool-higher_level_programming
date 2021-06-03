3169c897850154678d432a7f072f389b18a9dcc7
victor <vicuartas230@gmail.com>
2021-06-02 22:21:06 -0500

    Doing a test with open flag

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """ This function creates an Object from a “JSON file” """
-    with open(filename, 'r') as new:
+    with open(filename, 'a') as new:
         return json.load(new)

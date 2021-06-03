0e6e26411ebe5fc129112d466d0069a01b3b4a2f
victor <vicuartas230@gmail.com>
2021-06-02 23:15:46 -0500

    Fixing spell error in encoding

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,7 +4,7 @@
 
 def read_file(filename=""):
     """ This function reads a text file (UTF8) and prints it to stdout """
-    with open(filename, 'r', encoding='UTF8') as new:
+    with open(filename, 'r', encoding='utf-8') as new:
         reading = new.read()
     print(reading)
     new.close()

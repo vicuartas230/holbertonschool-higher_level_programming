63e5697e51e6da4aabf9a9db9ec67834cc8cdfe2
victor <vicuartas230@gmail.com>
2021-06-04 16:52:01 -0500

    Task 7 done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,8 +81,9 @@ class Rectangle(Base):
         return self.__height * self.__width
 
     def display(self):
+        print(self.__y * '\n', end='')
         for row in range(self.__height):
-            print(self.__width * '#')
+            print(self.__x * ' ' + self.__width * '#')
 
     def __str__(self):
         return "[{}] ({}) {}/{} - {}/{}\

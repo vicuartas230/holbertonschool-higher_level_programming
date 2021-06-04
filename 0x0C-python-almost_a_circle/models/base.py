8b4b2dc82738a58ecae08d4358e5fff6e43f2a30
victor <vicuartas230@gmail.com>
2021-06-04 13:45:59 -0500

    Fixing updating error

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,12 +2,13 @@
 """ This program defines a class Base """
 
 
-class base:
+class Base:
     """ This class will be the “base” of all other classes in this project. """
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id:
             self.id = id
         else:
-            self.__nb_objects += 1
+            Base.__nb_objects += 1
             self.id = self.__nb_objects

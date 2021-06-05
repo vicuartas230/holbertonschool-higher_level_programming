393a1b7b8a3cca7232d2e0089ecb180764d68a8e
victor <vicuartas230@gmail.com>
2021-06-04 23:14:50 -0500

    Task 15 done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ This program defines a class Base """
+import json
 
 
 class Base:
@@ -12,3 +13,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if not list_dictionaries:
+            return []
+        else:
+            return json.dumps(list_dictionaries)

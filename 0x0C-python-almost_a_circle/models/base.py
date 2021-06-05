08601e3161a8dc7a940e71f0ee1fd9c0f6ced33a
victor <vicuartas230@gmail.com>
2021-06-05 13:22:37 -0500

    Task 16 done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,8 +14,21 @@ class Base:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         if not list_dictionaries:
-            return []
+            return '[]'
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        list_dicts = []
+        if not list_objs:
+            with open('Rectangle.json', 'w') as fd:
+                json.dump(list_dicts, fd)
+        else:
+            with open('Rectangle.json', 'w') as fd:
+                for obj in list_objs:
+                    list_dicts.append(obj.__dict__)
+                json.dump(Base.to_json_string(list_dicts), fd)

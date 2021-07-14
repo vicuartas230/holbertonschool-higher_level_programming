284a6f115aea5ab4e16b7f31c81186fe621d34ed
vicuartas230 <vicuartas230@gmail.com>
2021-07-14 19:17:22 +0000

    Join the tird table

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,5 +3,7 @@ SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id=tv_show_genres.genre_id
-WHERE tv_show_genres.show_id LIKE 8
+INNER JOIN tv_shows
+ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title LIKE 'Dexter'
 ORDER BY tv_genres.name;

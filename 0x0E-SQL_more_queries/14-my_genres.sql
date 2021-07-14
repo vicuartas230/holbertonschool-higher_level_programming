c86be2993abe24e67da3655e34c88f7184a7ee6a
vicuartas230 <vicuartas230@gmail.com>
2021-07-14 18:47:03 +0000

    Add order by

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,4 +3,5 @@ SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id=tv_show_genres.genre_id
-WHERE tv_show_genres.show_id LIKE 8;
+WHERE tv_show_genres.show_id LIKE 8
+ORDER BY tv_genres.name;

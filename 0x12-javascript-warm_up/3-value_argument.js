1b8711a472ed63dbb9fefc8a98dd53e2f59999dc
victor <vicuartas230@gmail.com>
2021-08-02 12:33:53 -0500

    Remove length of the code

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const process = require('process');
 const args = process.argv;
-if (args.length === 2) {
+if (!args[2]) {
   console.log('No argument');
 } else {
   console.log(args[2]);

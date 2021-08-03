d50a0e549ec4c608b20a07436917e95d041cc6f6
victor <vicuartas230@gmail.com>
2021-08-03 18:25:31 -0500

    Remove the new line characters

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,7 +4,7 @@ const args = process.argv;
 const fs = require('fs');
 fs.readFile(args[2], 'utf-8', function (_err, data) {
   fs.readFile(args[3], 'utf-8', function (err2, data2) {
-    const str = data + '\n' + data2 + '\n';
+    const str = data + data2;
     fs.writeFile(args[4], str, function () {});
   });
 });

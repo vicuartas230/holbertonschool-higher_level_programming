6679ff1707f677f11d81a0cbf04864dc7227c267
victor <vicuartas230@gmail.com>
2021-08-04 13:41:45 -0500

    Upload script of task 12

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+let occurences = [];
+const a = Object.entries(dict);
+let b = Object.keys(newDict);
+let search = a[0][1];
+let i = 0;
+let j = 0;
+while (j < a.length) {
+  i = j;
+  if (!b.includes(search.toString())) {
+    while (i < a.length) {
+      if (b.includes(search.toString())) {
+        break;
+      }
+      if (a[i][1] === search) {
+        occurences.push(a[i][0]);
+      }
+      i++;
+    }
+    newDict[search] = occurences;
+    b = Object.keys(newDict);
+  }
+  j++;
+  if (j < a.length - 1) {
+    search = a[j][1];
+  }
+  occurences = [];
+}
+console.log(newDict);

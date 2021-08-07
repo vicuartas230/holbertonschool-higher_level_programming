2a45247ba82f2cbf70a9c18f0f2eb07cf0e84ba1
victor <vicuartas230@gmail.com>
2021-08-06 19:47:15 -0500

    Upload script of task 11 and add constructor method to model_state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,3 +12,6 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+    def __init__(self, name):
+        self.name = name

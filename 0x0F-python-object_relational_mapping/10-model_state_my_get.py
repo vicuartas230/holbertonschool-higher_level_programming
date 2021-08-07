08eb5fb7b7ba7149f1e7a104356e13d68c0d5eeb
victor <vicuartas230@gmail.com>
2021-08-06 19:32:34 -0500

    Upload the script of task 10 and change the connection arguments

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" This script lists all State objects that contain
+    the letter a from the database hbtn_0e_6_usa. """
+import sys
+
+from sqlalchemy.sql.expression import desc
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session, sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
+'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    consult = session.query(State).filter(
+              State.name.ilike(sys.argv[4])).order_by(State.id).all()
+    new = []
+    for a in consult:
+        new.append(a.name)
+    if sys.argv[4] not in new:
+        print('Not found')
+    else:
+        for state in consult:
+            print(state.id)
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" This script prints the first State object
-    from the database hbtn_0e_6_usa. """
+""" This script lists all State objects that contain
+    the letter a from the database hbtn_0e_6_usa. """
 import sys
+
+from sqlalchemy.sql.expression import desc
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session, sessionmaker
@@ -12,8 +14,7 @@ if __name__ == '__main__':
 '.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    consult = session.query(State).first()
-    if consult:
-        print("{}: {}".format(consult.id, consult.name))
-    else:
-        print('Nothing')
+    consult = session.query(State).filter(State.name.ilike('%a%\
+')).order_by(State.id).all()
+    for state in consult:
+        print("{}: {}".format(state.id, state.name))

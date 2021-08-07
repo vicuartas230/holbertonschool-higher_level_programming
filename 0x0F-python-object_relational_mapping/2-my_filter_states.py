20be19361c7e1f08bcaf87ae8399496cb9a5d21a
victor <vicuartas230@gmail.com>
2021-08-07 00:42:09 -0500

    Add binary to where

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,10 +7,10 @@ from sys import argv
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost\
-",user=argv[1], passwd=argv[2], db=argv[3])
+", user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     query = "SELECT * FROM states \
-WHERE name='{}' ORDER BY id ASC".format(argv[4])
+WHERE BINARY name='{}' ORDER BY id ASC".format(argv[4])
     cursor.execute(query)
     for state in cursor.fetchall():
         print(state)

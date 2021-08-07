99d0959fbf245e4d92e5a15d0b67322ce1e47e6d
victor <vicuartas230@gmail.com>
2021-08-06 19:12:38 -0500

    Upload script of task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-""" This script prints the first State object from the database hbtn_0e_6_usa. """
+""" This script prints the first State object
+    from the database hbtn_0e_6_usa. """
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session, sessionmaker
 
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format('root', 'Root#10.', 'hbtn_0e_6_usa'), pool_pre_ping=True)
-    query = engine.execute("SELECT * FROM states ORDER BY id LIMIT 1")
-    for state in query:
-        print("{}: {}".format(state[0], state[1]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
+'.format('root', 'Root#10.', 'hbtn_0e_6_usa'), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    consult = session.query(State).first()
+    if consult:
+        print("{}: {}".format(consult.id, consult.name))
+    else:
+        print('Nothing')

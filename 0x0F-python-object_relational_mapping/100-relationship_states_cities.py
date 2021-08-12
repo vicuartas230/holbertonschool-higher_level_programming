687e28b0ebbda13dc163e68fab269fb8bc6f690a
vicuartas230 <vicuartas230@gmail.com>
2021-08-12 14:17:31 -0500

    Edit the args in create engine

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import Session, sessionmaker
 
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
-'.format('root', 'Root#10.', 'hbtn_0e_100_usa'), pool_pre_ping=True)
+'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all()
     Session = sessionmaker(bind=engine)
     session = Session()

8175c4bc4ca9367f614afdc8de4065146c961a04
victor <vicuartas230@gmail.com>
2021-08-19 16:03:54 -0500

    Change method of command cut

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL and displays all HTTP methods the server will accept.
-curl -s -i -X OPTIONS 0.0.0.0:5000/route_4 | grep Allow | cut -d' ' -f 2,3,4
+curl -s -i -X OPTIONS 0.0.0.0:5000/route_4 | grep Allow | cut -d' ' -f 2-

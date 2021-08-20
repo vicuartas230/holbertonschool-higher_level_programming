933221ffbc5b5edd803b7ae7986df167219c184a
victor <vicuartas230@gmail.com>
2021-08-20 18:42:22 -0500

    Upload script of task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This script takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter. """
+from requests import post
+from sys import argv
+
+
+if __name__ == '__main__':
+    if len(argv) < 2:
+        dicc = {'q': ""}
+    else:
+        dicc = {'q': argv[1]}
+    req = post('http://0.0.0.0:5000/search_user', data=dicc)
+    if len(req.json()) != 0 and type(req.json()) is dict:
+        print("[{}] {}".format(req.json()['id'], req.json()['name']))
+    elif type(req.json()) is not dict:
+        print("Not a valid JSON")
+    else:
+        print("No result")

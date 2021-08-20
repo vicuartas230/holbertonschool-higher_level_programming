eddf1f05a7ed9e82d687106fb3c809f20dd63fd8
victor <vicuartas230@gmail.com>
2021-08-19 20:18:16 -0500

    Use binary search

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,14 +7,17 @@ def find_peak(list_of_integers):
     length = len(list_of_integers)
     if not length:
         return None
-    if max(list_of_integers) == min(list_of_integers):
-        return list_of_integers[0]
-    if list_of_integers[0] >= list_of_integers[1]\
-       and not (list_of_integers[0] < max(list_of_integers)):
-        return list_of_integers[0]
-    if list_of_integers[length - 1] >= list_of_integers[length - 2]:
-        return list_of_integers[length - 1]
-    for i in range(1, length - 1):
-        if list_of_integers[i] > list_of_integers[i - 1]\
-           and list_of_integers[i] > list_of_integers[i + 1]:
-            return list_of_integers[i]
+    return peak_recursion(list_of_integers, 0, length - 1, length)
+
+
+def peak_recursion(list, lower, higher, length):
+    """ This function does a binary search with recursion """
+    middle = lower + (higher - lower) / 2
+    middle = int(middle)
+    if ((middle == 0 or list[middle - 1] <= list[middle]) and
+       (middle == length - 1 or list[middle + 1] <= list[middle])):
+        return list[middle]
+    elif middle > 0 and list[middle - 1] > list[middle]:
+        return peak_recursion(list, lower, (middle - 1), length)
+    else:
+        return peak_recursion(list, (middle + 1), higher, length)

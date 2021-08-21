0ebc19e5121bf9a4aed3949a2502f890165758c5
victor <vicuartas230@gmail.com>
2021-08-20 20:48:44 -0500

    Add try except for errors

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,6 +8,9 @@ if __name__ == '__main__':
     req = get("https://api.github.com/repos/{}/{}/commits\
 ".format(argv[2], argv[1]))
     # print(req.json()[0])
-    for i in range(0, 10):
-        print("{}: {}".format(req.json()[i]['sha'], req.json()[i]['\
+    try:
+        for i in range(0, 10):
+            print("{}: {}".format(req.json()[i]['sha'], req.json()[i]['\
 commit']['author']['name']))
+    except:
+        print('', end='')

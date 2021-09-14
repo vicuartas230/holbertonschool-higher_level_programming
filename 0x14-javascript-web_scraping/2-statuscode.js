db5c9717272504f2cae71740c08b13c67c4b5d2d
victor <vicuartas230@gmail.com>
2021-09-14 12:34:37 -0500

    Change variable name

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,9 @@
 const request = require('request');
 const argv = process.argv;
 
-request(argv[2], function (err, response) {
-  if (err) {
-    console.log(err);
-  } else {
+request(argv[2], function (error, response) {
+    if (error) {
+        console.log(error)
+    }
     console.log('code: ', response.statusCode);
-  }
 });

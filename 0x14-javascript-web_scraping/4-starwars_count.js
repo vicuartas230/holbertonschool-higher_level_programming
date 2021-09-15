db57335f29ea19b7e27f1c096a5146cdc76d4486
vicuartas230 <vicuartas230@gmail.com>
2021-09-14 23:11:14 -0500

    Use regex to filter the id

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,7 +8,7 @@ request(argv[2], function (error, response, body) {
   }
   const listFilms = JSON.parse(body).results;
   const film = listFilms.map(item => item.characters);
-  const chars = film.map(x => x.includes('https://swapi-api.hbtn.io/api/people/18/'));
+  const chars = film.map(x => x.toString().match(/18/g));
   const done = chars.filter(element => element).length;
   console.log(done);
 });

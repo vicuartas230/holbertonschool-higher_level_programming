49657aeede7c05623ac0b381387c812f31b427bc
vicuartas230 <vicuartas230@gmail.com>
2021-09-14 22:15:25 -0500

    Fix style errors

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,14 +7,14 @@ request(argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   }
-  task = JSON.parse(body).filter(function (item) {
-      return item.completed === true;
+  const task = JSON.parse(body).filter(function (item) {
+    return item.completed === true;
   });
   for (let index = 0; index < task.length; index++) {
     if (dicc[task[index].userId]) {
-        dicc[task[index].userId]++;
+      dicc[task[index].userId]++;
     } else {
-        dicc[task[index].userId] = 1;
+      dicc[task[index].userId] = 1;
     }
   }
   console.log(dicc);
